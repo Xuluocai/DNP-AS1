94cc9ee532a1feeb1f739408d027994153891fc5
Xuluocai <56256123+Xuluocai@users.noreply.github.com>
2020-10-21 21:38:27 +0200

    Create User.cs

---
+++ Models/User.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Assignment.Models
+{
+    public class User
+    {
+      
+        public String name { get; set; }
+       
+   
+        public String password { get; set; }
+        public String CPassword { get; set; }
+        public int ID { get; set; }
+
+        public User(String name,String password)
+        {
+            this.name = name;
+            this.password = password;
+        }
+    }
+}
