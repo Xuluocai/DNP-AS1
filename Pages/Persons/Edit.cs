5fa1ed17d8233892db6a5bcd2761a0f4cd16c4e7
Xuluocai <56256123+Xuluocai@users.noreply.github.com>
2020-10-21 21:45:40 +0200

    Create Edit.cs

---
+++ Pages/Persons/Edit.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+
+namespace Assignment.Models
+{
+    public class Edit:PageModel
+    {
+        private readonly Models.PersonContext _context;
+
+        public Edit(Models.PersonContext context)
+        {
+            _context = context;
+        }
+
+        [BindProperty]
+        public Person Person { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Person = await _context.Person.FirstOrDefaultAsync(m => m.Id == id);
+
+            if (Person == null)
+            {
+                return NotFound();
+            }
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _context.Attach(Person).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!PersonExists(Person.Id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return RedirectToPage("./View");
+        }
+
+        private bool PersonExists(int id)
+        {
+            return _context.Person.Any(e => e.Id == id);
+        }
+    }
+}
