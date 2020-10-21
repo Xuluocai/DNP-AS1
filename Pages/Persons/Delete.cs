6b98fb00fd7d99d756cee537d7c84333e75d5374
Xuluocai <56256123+Xuluocai@users.noreply.github.com>
2020-10-21 21:46:15 +0200

    Create Delete.cs

---
+++ Pages/Persons/Delete.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+
+namespace Assignment.Models
+{
+    public class Delete:PageModel
+    {
+        private readonly Models.PersonContext _context;
+
+        public Delete(Models.PersonContext context)
+        {
+            _context = context;
+        }
+
+        [BindProperty]
+        public Person Person { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Person = await _context.Person.FirstOrDefaultAsync(m => m.Id == id);
+
+            if (Person == null)
+            {
+                return NotFound();
+            }
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Person = await _context.Person.FindAsync(id);
+
+            if (Person != null)
+            {
+                _context.Person.Remove(Person);
+                await _context.SaveChangesAsync();
+            }
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
+
