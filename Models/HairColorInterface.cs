2f433a9883b5c7e2bf6390a2037188b52d8378b5
Xuluocai <56256123+Xuluocai@users.noreply.github.com>
2020-10-21 21:38:50 +0200

    Create HairColorInterface.cs

---
+++ Models/HairColorInterface.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Assignment.Models
+{
+    interface HairColorInterface
+    {
+        public String SelectHairColor(int i);
+        public String RandomHairColor();
+        public String[] getHairColor();
+    }
+}
