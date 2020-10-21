ae34ab49b2d84d6f15fb9f11aa4c09532b5c8f3a
Xuluocai <56256123+Xuluocai@users.noreply.github.com>
2020-10-21 21:38:33 +0200

    Create PersonContext.cs

---
+++ Models/PersonContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace Assignment.Models
+{
+    public class PersonContext:DbContext
+    {
+        public PersonContext(DbContextOptions<PersonContext> options): base(options) { }
+
+        public DbSet<Assignment.Models.Person> Person { get; set; }
+    }
+}
