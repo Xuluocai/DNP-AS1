660cef45c7f90172f6ed32cf22d3a9642cee41b5
Xuluocai <56256123+Xuluocai@users.noreply.github.com>
2020-10-21 21:39:01 +0200

    Create EyeColorInterface.cs

---
+++ Models/EyeColorInterface.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Assignment.Models
+{
+    interface EyeColorInterface
+    {
+        public String SelectEyeColor(int index);
+        public String RandomColor();
+        public String[] getEyeColor();
+    }
+}
