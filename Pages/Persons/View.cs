e53e117682833e910908c27abfef693a8fcc7119
Xuluocai <56256123+Xuluocai@users.noreply.github.com>
2020-10-21 21:45:33 +0200

    Create View.cs

---
+++ Pages/Persons/View.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+
+namespace Assignment.Models
+{
+    public class View:PageModel
+    {
+        private readonly Models.PersonContext _context;
+
+        public View(Models.PersonContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Person> Person { get; set; }
+
+        public async Task OnGetAsync()
+        {
+            Person = await _context.Person.ToListAsync();
+        }
+
+    }
+}
