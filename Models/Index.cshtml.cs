bb0610bff442a0cc5c4504230bbbb9524b5f435b
Xuluocai <56256123+Xuluocai@users.noreply.github.com>
2020-10-21 21:38:44 +0200

    Create Index.cshtml.cs

---
+++ Models/Index.cshtml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Assignment.Data;
+
+namespace Assignment.Models
+{
+    public class IndexModel : PageModel
+    {
+        private readonly Assignment.Data.AssignmentContext _context;
+
+        public IndexModel(Assignment.Data.AssignmentContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Person> Person { get;set; }
+
+        public async Task OnGetAsync()
+        {
+            Person = await _context.Person.ToListAsync();
+        }
+    }
+}
