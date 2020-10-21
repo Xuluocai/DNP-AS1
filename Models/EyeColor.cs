c3ffd947e44fbae0a4ea3399a0b8b041047f898b
Xuluocai <56256123+Xuluocai@users.noreply.github.com>
2020-10-21 21:39:05 +0200

    Create EyeColor.cs

---
+++ Models/EyeColor.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Assignment.Models
+{
+    public class EyeColor : EyeColorInterface
+    { String[] eyeColorf;
+        public EyeColor() {
+            String[] eyeColor = { "Black", "Brown", "Grey", "Green", "Blue", "Amber", "Hazel" };
+        this.eyeColorf = eyeColor; }
+        public String SelectEyeColor(int index)
+        {
+            Console.WriteLine("eyecolour selected in class" + eyeColorf[index]);
+            return eyeColorf[index];
+        }
+        public String RandomColor()
+        {
+            Random rnd = new Random();
+            int index = rnd.Next(0, 6);
+            Console.WriteLine("rnd in eye Color:" + index);
+            Console.WriteLine(eyeColorf[index]);
+            return eyeColorf[index];
+        }
+        public String[] getEyeColor(){
+            return eyeColorf;}
+    }
+}
